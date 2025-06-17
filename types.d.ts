030715410e5babffb4da1432916f01d0b1a1a8a1
Noor-Tanvir-Hossain <tanvirrobin11567@gmail.com>
2025-06-17 11:07:50 +0600

    project initialize

---
+++ types.d.ts
@@ -0,0 +1,37 @@
+export interface Iuser {
+  _id: string;
+  name: string;
+  email: string;
+  password?: string;
+  profilePicture?: string;
+  bio?: string;
+  followers?: string[];
+  following?: string[];
+  posts?: IPost[];
+  savePosts?: string[] | IPost[];
+  isVarified: boolean;
+}
+
+export interface IComment {
+  _id: string;
+  text?: string;
+  user: {
+    _id: string;
+    name: string;
+    profilePicture: string;
+  };
+  createdAt: string;
+}
+
+export interface Ipost {
+    _id:string
+  caption?: string; 
+  image: {
+    url: string;
+    publicId: string;
+  };
+  user: Iuser | undefined;
+  likes: string[];
+  comments:IComment[];
+  createdAt: string;
+}
